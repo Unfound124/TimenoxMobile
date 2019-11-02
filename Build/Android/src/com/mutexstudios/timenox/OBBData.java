181fc1a248076f461dde5979370c67605ac969d9
Vijay Duggirala <vduggirala@csu.fullerton.edu>
2019-11-01 18:40:27 -0700

    Fixed Logging-in bug

--- Build/Android/src/com/mutexstudios/timenox/OBBData.java
+++ Build/Android/src/com/mutexstudios/timenox/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"7", // the version of the APK that the file was uploaded against
-161058541L // the length of the file in bytes
+"8", // the version of the APK that the file was uploaded against
+161058233L // the length of the file in bytes
 )
 };
 };

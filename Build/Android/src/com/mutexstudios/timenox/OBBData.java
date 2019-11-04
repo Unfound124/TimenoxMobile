2253e0dbb20fef125b129bc7e201324490373db3
Vijay Duggirala <vduggirala@csu.fullerton.edu>
2019-11-04 05:30:54 -0800

    Fixing Save

--- Build/Android/src/com/mutexstudios/timenox/OBBData.java
+++ Build/Android/src/com/mutexstudios/timenox/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"8", // the version of the APK that the file was uploaded against
-161058233L // the length of the file in bytes
+"13", // the version of the APK that the file was uploaded against
+166690189L // the length of the file in bytes
 )
 };
 };

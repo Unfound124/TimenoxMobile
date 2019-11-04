d5175b2914a072482fb51544470d9c697e21cb1d
Vijay Duggirala <vduggirala@csu.fullerton.edu>
2019-11-04 05:49:35 -0800

    tried another fix

--- Build/Android/src/com/mutexstudios/timenox/OBBData.java
+++ Build/Android/src/com/mutexstudios/timenox/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"13", // the version of the APK that the file was uploaded against
-166690189L // the length of the file in bytes
+"14", // the version of the APK that the file was uploaded against
+166686660L // the length of the file in bytes
 )
 };
 };
